5fc5c5b3c47eb5345c1fab95d777baecae1dff68
Daniel <dcochran@netflix.com>
2021-04-06 17:46:05 -0700

    still reproducing

--- src/index.tsx
+++ src/index.tsx
@@ -16,23 +16,7 @@ const createStudioGraphqlClient = () => {
         // Read from persisted cache at first when opening app, but kick off a network
         // request to get updated data.
         fetchPolicy: 'cache-and-network',
-        nextFetchPolicy(lastFetchPolicy) {
-          if (lastFetchPolicy === 'cache-and-network' || lastFetchPolicy === 'network-only') {
-            return 'cache-first';
-          }
-          return lastFetchPolicy;
-        },
-  
-        // By default, mutation calls to `refetchQueries` do not trigger re-renders, e.g.
-        // the loading state would be silent. We don't want that. We want loading states.
-        notifyOnNetworkStatusChange: true,
-        // refetchWritePolicy: 'overwrite',
       },
-    },
-    cacheConfig: {
-      typePolicies: {
-
-      }
     },
     namedLinks: {
       corsLink: false,
